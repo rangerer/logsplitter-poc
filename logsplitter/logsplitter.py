db5dce5979bbe8933640e863074bf072b0688ef3
Roland Angerer <dev@rangerer.at>
2020-03-07 20:11:04 +0100

    improve test performance

--- logsplitter/logsplitter.py
+++ logsplitter/logsplitter.py
@@ -9,20 +9,22 @@ def error(message):
 
 bootstrap_servers = ["kafka:9092"]
 
-# connect consumer process to test topic (10 retries w/ 30s delay = 5 minutes)
-for i in range(10):
+# connect consumer process to test topic (60 retries w/ 5s delay = 5 minutes)
+for i in range(60):
     try:
         consumer = KafkaConsumer("test", bootstrap_servers=bootstrap_servers)
         break
     except NoBrokersAvailable:
-        error("unable to connect to bootstrap servers {} - retrying in 30 seconds".format(bootstrap_servers))
-        sleep(30)
+        error("unable to connect to bootstrap servers {} - retrying in 5 seconds".format(bootstrap_servers))
+        sleep(5)
 else:
     error("unable to connect to bootstrap servers {} for 5 minutes - stopping".format(bootstrap_servers))
     quit(100)
 
 # connect producer process
 producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+
+print("logsplitter connected and waiting for input")
 
 # receive message from test and filter into test_hello topic
 for message in consumer:
--- test.sh
+++ test.sh
@@ -12,9 +12,10 @@ awesome
 why not
 EOT
 
+echo
 echo "read messages from test_hello topic"
 $KAFKA_HOME/bin/kafka-console-consumer.sh \
     --bootstrap-server kafka:9092 \
     --topic test_hello \
     --from-beginning \
-    --timeout-ms 1000
+    --timeout-ms 5000
